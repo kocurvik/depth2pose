bf26888cfa5a1c10aefbe7a3b35f548273eec65a
Viktor Kocur <kocurvik@gmail.com>
2026-04-01 22:16:41 +0200

    * pose eval slurm script

--- slurm_scripts/eval_mdrpbench.sh
+++ slurm_scripts/eval_mdrpbench.sh
@@ -4,16 +4,34 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
-#SBATCH -t 1:00:00
+#SBATCH -t 4:00:00
 #SBATCH --mem 120GB
-#SBATCH -o /home/kocurvik/logs/eval_mdrpbench.000.std.out
-#SBATCH -e /home/kocurvik/logs/eval_mdrpbench.000.err.out
+#SBATCH -o /home/kocurvik/logs/eval_mdrpbench.001.std.out
+#SBATCH -e /home/kocurvik/logs/eval_mdrpbench.001.err.out
 
 # Go to your project directory
 cd /home/kocurvik/code/mdrpbench
 export PYTHONPATH=/home/kocurvik/code/mdrpbench
-#export PATH="/home/kocurvik/mambaforge/envs/mdrpbench/bin:$PATH"
 module load git
 
+SRC=/home/kocurvik/data/mdrpbench/pt
+WORK=/work/$SLURM_JOB_ID/mdrpbench/pt
+
+
+# Copy data to work
+echo "Copying data to /work..."
+mkdir -p "$WORK"
+rsync -a "$SRC/" "$WORK/"
+
+# Run processing on /work
+python eval_pose.py -ss -nw 64 WORK pt splg_2048_noresize
+
+# Copy results back
+echo "Copying results back..."
+rsync -a "$WORK/" "$SRC/"
+# rsync -a "$DATASET_WORK/" "$DATASET_SRC/"
+
+echo "Done."
+
 # Run your scripts using that specific Python executable
-python eval_pose.py -ss -nw 64 /home/kocurvik/data/mdrpbench/pt pt splg_2048_noresize
+
