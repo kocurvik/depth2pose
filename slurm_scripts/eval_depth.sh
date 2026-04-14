ec88cb454e42f1109581695e21d066fa1182b9cc
Viktor Kocur <kocurvik@gmail.com>
2026-04-14 19:46:27 +0200

    * add slurm script for eval_depth

---
+++ slurm_scripts/eval_depth.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account p1358-25-2
+#SBATCH -p gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH -G 1
+#SBATCH -t 4:00:00
+#SBATCH --mem 64GB
+#SBATCH -o /home/kocurvik/logs/eval_depth.000.std.out
+#SBATCH -e /home/kocurvik/logs/eval_depth.000.err.out
+
+# Go to your project directory
+cd /home/kocurvik/code/mdrpbench
+export PYTHONPATH=/home/kocurvik/code/mdrpbench
+module load git
+
+# Run processing on /work
+python eval_depth.py /home/kocurvik/mdrpbench/all_results ./datasets/devana_benchmarks.json --device cuda
+
+echo "Done."
+
+
